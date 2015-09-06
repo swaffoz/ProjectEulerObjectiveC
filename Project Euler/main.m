df3398be3d34d4799ae4124d79d681501e657d2b
Zane Swafford <zane@zaneswafford.com>
2015-09-06 13:33:18 -0500

    Initial Commit

---
+++ Project Euler/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Project Euler
+//
+//  Created by Zane Swafford on 9/6/15.
+//  Copyright (c) 2015 Zane Swafford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
